bcbd2b80b11c19ac581df38f441faf332ecdb1c4
Kevin Eye <eye@buffalo.edu>
2020-02-12 05:53:51 -0500

    added timelapse/warp script

--- cpanfile
+++ cpanfile
@@ -1,3 +1,4 @@
 requires 'Future::AsyncAwait', '0.36';
 requires 'Mojo::IOLoop::ProcBackground', '0.06';
 requires 'Mojolicious', '8.32';
+requires 'YAML', '1.29';
---
+++ lib/IPCam/WarpVideo.pm
@@ -0,0 +1,85 @@
+package IPCam::WarpVideo;
+use Mojo::Base -base, -strict, -signatures, -async_await;
+
+use Mojo::IOLoop::ProcBackground;
+use Mojo::File 'path';
+use Mojo::Log;
+
+has camera => undef;
+has dir => undef;
+has interval => 300;
+has log => sub {Mojo::Log->new};
+
+sub start($self) {
+  Mojo::IOLoop->recurring($self->interval => sub { $self->tick });
+  $self->tick;
+}
+
+async sub tick($self) {
+  eval {
+    await $self->snap;
+    my $time = time;
+    my @l = localtime;
+    # if ($l[1] * 60 + $l[0] < $self->interval) {
+      await $self->warp_day($time - $self->interval);
+      my $month_file = await $self->warp_month($time - $self->interval);
+      my $latest = sprintf "%s/latest.mp4", $self->dir;
+      unlink $latest;
+      my $latest_target = $month_file;
+      substr($latest_target, 0, length($self->dir)+1) = '' if substr($latest_target, 0, length $self->dir) eq $self->dir;
+      symlink $latest_target => $latest;
+    # }
+  };
+  if ($@) {
+    $self->log->error($@);
+  }
+}
+
+async sub snap($self) {
+  my @l = localtime;
+  my $file = sprintf '%s/%04d/%02d/%02d-%02d%02d%02d.jpg', $self->dir, $l[5]+1900, $l[4]+1, @l[3,2,1,0];
+  my $dir = sprintf '%s/%04d/%02d', $self->dir, $l[5]+1900, $l[4]+1;
+  path($dir)->make_path;
+  $self->log->debug("snapping $file");
+  await $self->camera->cmd_monitor($file, 0.1);
+  return $file;
+}
+
+async sub warp_day($self, $time) {
+  my @l = localtime $time;
+  my $glob = sprintf '%s/%04d/%02d/%02d-*.jpg', $self->dir, $l[5]+1900, $l[4]+1, $l[3];
+  my $out = sprintf '%s/%04d/%02d/%02d.mp4', $self->dir, $l[5]+1900, $l[4]+1, $l[3];
+  my $p = Mojo::Promise->new;
+  my $proc = Mojo::IOLoop::ProcBackground->new;
+  $proc->on(dead => sub {
+    $p->resolve($out)});
+  $self->log->debug("warping daily photos to $out");
+  $proc->run('</dev/null ' . join ' ', map { _shell_quote($_) } qw(ffmpeg -loglevel error -hide_banner -framerate 30 -pattern_type glob -i), $glob, qw(-c:v libx264 -pix_fmt yuv420p -y), $out);
+  return $p;
+}
+
+async sub warp_month($self, $time) {
+  my @l = localtime $time;
+  my @files = glob sprintf '%s/%04d/%02d/*.mp4', $self->dir, $l[5]+1900, $l[4]+1;
+  my $list = path(File::Temp->new(UNLINK => 0));
+  $list->spurt(join '', map { "file '" . path($_)->to_abs . "'\n"} @files);
+  my $out = sprintf '%s/%04d/%02d.mp4', $self->dir, $l[5]+1900, $l[4]+1;
+  my $p = Mojo::Promise->new;
+  my $proc = Mojo::IOLoop::ProcBackground->new;
+  $proc->on(dead => sub {
+    $list->remove;
+    $p->resolve($out)});
+  $self->log->info("warping month to $out");
+  $proc->run('</dev/null ' . join ' ', map { _shell_quote($_) } qw(ffmpeg -loglevel error -hide_banner -f concat -safe 0 -i), $list, qw(-c copy -y), $out);
+  return $p;
+}
+
+sub _shell_quote($arg) {
+  if ($arg =~ /\A\w+\z/) {
+    return $arg;
+  }
+  $arg =~ s/'/'"'"'/g;
+  return "'$arg'";
+}
+
+1;
